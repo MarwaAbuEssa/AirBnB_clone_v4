ff9ec3636052fa73f95c3a4ca689dcba02db0f5d
MarwaAbuEssa <meromeroo@gmail.com>
2024-02-10 23:42:28 +0200

    clne air bn v3

---
+++ tests/test_console.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Contains the class TestConsoleDocs
+"""
+
+import console
+from contextlib import contextmanager
+from datetime import datetime
+import inspect
+from io import StringIO
+import models
+import pep8
+import sys
+from os import environ, stat
+HBNBCommand = console.HBNBCommand
+
+Place = models.Place
+State = models.State
+User = models.User
+STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
+HBNBCommand = console.HBNBCommand
+storage = console.storage
+CNC = models.CNC
+
+
+class TestConsoleDocs(unittest.TestCase):
+    """Class for testing documentation of the console"""
+    def test_pep8_conformance_console(self):
+        """Test that console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_console(self):
+        """Test that tests/test_console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_console_module_docstring(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
+
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
